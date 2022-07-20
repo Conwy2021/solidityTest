850b40a023a9ef7c422156eaa1b1073ce3d177d1
"Conwy2021" <"16602889867@163.com">
2022-07-20 15:28:09 +0800

    字符0 和数字 0 知识汇总

--- jichuyufaTest/cuncu/keccak256.sol
+++ jichuyufaTest/cuncu/keccak256.sol
@@ -7,12 +7,13 @@ contract keccak256Test{
         
         bytes32 kec=keccak256('0');
         
-        console.logBytes32(kec);
+        console.logBytes32(kec);//0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d
     }
 
-    function keccakTest2() public view {
-        console.logBytes32(bytes32(0));
-        bytes32 kec=keccak256(bytes32(0));//这个是和网上一致的 https://learnblockchain.cn/books/geth/part7/storage.html
+    function keccakTest2(uint aa) public view {
+        console.logBytes32(bytes32(aa));
+        bytes32 kec=keccak256(bytes32(aa));//这个是和网上一致的 https://learnblockchain.cn/books/geth/part7/storage.html  
+                                            //keccak256(0)=0x290decd9548b62a8d60345a988386fc84ba6bc95484008f6362f93160ef3e563
         
         console.logBytes32(kec);
 
@@ -20,9 +21,17 @@ contract keccak256Test{
 
     function keccakTest21(string a) public view {
         
-        bytes32 kec=keccak256(a);//这个是和网上一致的 https://learnblockchain.cn/books/geth/part7/storage.html
+        bytes32 kec=keccak256(a);
         
         console.logBytes32(kec);// '0' ->  0x044852b2a670ade5407e78fb2863c51de9fcb96542a07186fe3aeda6bb8a116d
+                                // 'a' -> 0x3ac225168df54212a25c1c01fd35bebfea408fdac2e31ddd6f80a4bbf9a5f1cb
+    }
+
+    function keccakTest30(uint aa) public view {
+        console.logBytes1(bytes1(aa));
+        bytes32 kec=keccak256(bytes1(aa));
+        
+        console.logBytes32(kec);
 
     }
 }
