918aa8b8c7b73e06f4f01975af79661242a3a87c
"Conwy2021" <"16602889867@163.com">
2022-07-22 10:31:41 +0800

    new 合约使用 测试

--- jichuyufaTest/nowTest.sol
+++ jichuyufaTest/nowTest.sol
@@ -2,14 +2,10 @@
 pragma solidity ^0.8.0; 
 
 import 'hardhat/console.sol';
+import 'jichuyufaTest/test.sol';
 contract nowTest{
-
-    function test() public{
-
-        
-        uint time = block.timestamp;//所以每个块都有一个时间戳（创建时） 所以时创建块的时间戳
-        console.log(time);
-        uint time2 = block.timestamp;
-        console.log(time2);
+        HelloWeb3 public hello;
+        constructor() public{
+        hello=new HelloWeb3();
     }
 }
