19a6ac888f67d81562fb5eaf12ef6f7afe991329
"Conwy2021" <"16602889867@163.com">
2022-07-26 14:45:43 +0800

    add Delegation靶场及过关思路(调整钱包限制gas上限 我一般直接加个0)

---
+++ bachang/Delegation.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract Delegate {
+
+  address public owner;
+
+  constructor(address _owner) public {
+    owner = _owner;
+  }
+
+  function pwn() public {
+    owner = msg.sender;
+  }
+}
+contract Delegation {
+
+  address public owner;
+  Delegate delegate;
+
+  constructor(address _delegateAddress) public {
+    delegate = Delegate(_delegateAddress);
+    owner = msg.sender;
+  }
+
+  fallback() external {
+    (bool result,) = address(delegate).delegatecall(msg.data);//传0xdd365b8b 后 在钱包 gas消耗 加个0 不然会失败因为gas限制太低
+    if (result) {
+      this;
+    }
+  }
+}
