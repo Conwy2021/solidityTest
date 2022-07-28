1004d1a40ae39548877e792e90ebfe74cf9aedba
"Conwy2021" <"16602889867@163.com">
2022-07-28 17:34:11 +0800

    计量单位 测试  只用wei 其他的是工具自己转换的

---
+++ jichuyufaTest/danWeiTest.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0; 
+
+import 'hardhat/console.sol';
+
+contract danWeiTest{
+
+    function test() public {
+
+        uint256 i =1 wei;//单位只有这个wei
+
+    }
+
+}
