497724f2529b4668e9dcb04e2957246ec5c29e0a
"Conwy2021" <"16602889867@163.com">
2022-08-03 00:13:01 +0800

    修改address 需要添加payable

--- jichuyufaTest/CallContract.sol
+++ jichuyufaTest/CallContract.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import { OtherContract } from "contracts/jiaoyi/OtherContract.sol";
+import { OtherContract } from "jichuyufaTest/OtherContract.sol";
 
 contract CallContract{
-    function callSetX(address _Address, uint256 x) external{
+    function callSetX(address payable _Address, uint256 x) external{
         OtherContract(_Address).setX(x);
     }
 
@@ -12,12 +12,12 @@ contract CallContract{
         x = _Address.getX();
     }
 
-    function callGetX2(address _Address) external view returns(uint x){
+    function callGetX2(address payable _Address) external view returns(uint x){
         OtherContract oc = OtherContract(_Address);
         x = oc.getX();
     }
 
-    function setXTransferETH(address otherContract, uint256 x) payable external{
+    function setXTransferETH(address payable otherContract, uint256 x) payable external{
         OtherContract(otherContract).setX{value: msg.value}(x);
     }
 }
